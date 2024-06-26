85d00eef4254e4071305eb965d582a0d286d5179
alex-delia <adelia3@uwo.ca>
2024-06-25 23:04:20 -0400

    create signup route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Members Only Club' });
 });
+
+/* GET sign up. */
+router.get('/signup', function (req, res, next) {
+  res.render('signup', { title: 'Sign Up' });
+});
+
 
 module.exports = router;
