d0c32b18e4bdb2d5dc766df7ff6c2c41f1a802a3
alex-delia <adelia3@uwo.ca>
2024-06-26 21:25:22 -0400

    create route to signup GET and POST

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const userController = require('../controllers/userController');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Members Only Club' });
 });
 
 /* GET sign up. */
-router.get('/signup', function (req, res, next) {
-  res.render('signup', { title: 'Sign Up' });
-});
+router.get('/signup', userController.signup_form_get);
+//POST request for signup
+router.post('/signup', userController.signup_form_post);
 
 
 module.exports = router;
